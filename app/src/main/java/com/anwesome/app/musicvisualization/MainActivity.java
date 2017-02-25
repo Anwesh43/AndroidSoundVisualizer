567737f969df243af2a7c202e1e11620579372cc
Anwesh43 <anweshthecool0@gmail.com>
2017-02-25 21:53:33 +0530

    Using VisualizingPlayer to visualize music

---
+++ app/src/main/java/com/anwesome/app/musicvisualization/MainActivity.java
@@ -0,0 +1,25 @@
+package com.anwesome.app.musicvisualization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.app.soundvisualization.VisualizingPlayer;
+
+public class MainActivity extends AppCompatActivity {
+    VisualizingPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        player = new VisualizingPlayer(this,"surc.mp3");
+        player.play();
+    }
+    public void onPause() {
+        super.onPause();
+        player.pause();
+    }
+    public void onResume() {
+        super.onResume();
+        player.resume();
+    }
+}
