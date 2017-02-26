25e27a7c4d17f238668f45aa47ab9b8a6c40e79f
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 11:34:16 +0530

    Visualizng View to use new visualization graph instead of itself drawing the visualization

--- soundvisualization/src/main/java/com/anwesome/app/soundvisualization/VisualizingView.java
+++ soundvisualization/src/main/java/com/anwesome/app/soundvisualization/VisualizingView.java
@@ -4,66 +4,46 @@ import android.content.Context;
 import android.graphics.*;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.anwesome.app.soundvisualization.graphs.BarGraph;
+import com.anwesome.app.soundvisualization.graphs.VisualizingGraph;
+import com.anwesome.app.soundvisualization.graphs.VisualizingGraphType;
 
 /**
  * Created by anweshmishra on 25/02/17.
  */
 public class VisualizingView extends View {
-    private int maxLimit = 256;
     private boolean updating = false;
+    private VisualizingGraph visualizingGraph;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private List<Integer> soundData = new ArrayList<>();
-    public VisualizingView(Context context) {
+    public VisualizingView(Context context,VisualizingGraphType visualizingGraphType) {
         super(context);
+        initVisualizngGraph(visualizingGraphType);
     }
-    public void setMaxLimit(int maxLimit){
-        if(maxLimit!=0) {
-            this.maxLimit = maxLimit;
+    public void initVisualizngGraph(VisualizingGraphType visualizingGraphType) {
+        switch (visualizingGraphType) {
+            case BARGRAPH:
+                this.visualizingGraph = new BarGraph();
+                break;
+            case CIRCLEGRAPH:
+                break;
+            default:
+                break;
         }
     }
     public boolean isUpdating() {
         return updating;
     }
     public void updateSoundData(byte[] fftData) {
-
-        if(fftData.length > 2) {
-            soundData.add(new Integer((int)Math.log10(fftData[0])*10));
-            soundData.add(new Integer((int)Math.log10(fftData[1])));
-            int max = Math.max(soundData.get(0),soundData.get(1));
-            for (int i = 2;i<fftData.length;i+=2 ) {
-                int re = fftData[i],im = fftData[i+1];
-                int f = (int)(10*Math.log10(re*re+im*im));
-                if(f>max) {
-                    max = f;
-                }
-                if(f == 0) {
-                    continue;
-                }
-                soundData.add(f);
-            }
-            maxLimit = max;
+        if(visualizingGraph!=null) {
+            visualizingGraph.update(fftData);
+            updating = true;
+            invalidate();
         }
-        updating = true;
-        invalidate();
     }
     public void onDraw(Canvas canvas) {
-        int h = canvas.getHeight()/2;
-        paint.setColor(Color.parseColor("#e53935"));
-        paint.setStrokeWidth(canvas.getWidth()/20);
-        canvas.drawLine(0,h,canvas.getWidth(),h,paint);
-        if(soundData.size()>0 && maxLimit!=0) {
-            float gap = canvas.getWidth()/soundData.size();
-            float x = gap/2;
-            paint.setColor(Color.parseColor("#00BCD4"));
-            for (Integer data : soundData) {
-                int y = h - ((data*h)/maxLimit);
-                canvas.drawLine(x,h,x,y,paint);
-                x+=gap;
-            }
+        if(visualizingGraph!=null) {
+            visualizingGraph.draw(canvas,paint);
             updating = false;
-            soundData = new ArrayList<>();
         }
     }
 }
