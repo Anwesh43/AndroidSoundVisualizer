cf9d62db3c5dd6215692d95e2f4aa9aa87621b7b
Anwesh43 <anweshthecool0@gmail.com>
2017-02-26 11:35:14 +0530

    Enum for visualization graph type

--- soundvisualization/src/main/java/com/anwesome/app/soundvisualization/graphs/VisualizingGraphType.java
+++ soundvisualization/src/main/java/com/anwesome/app/soundvisualization/graphs/VisualizingGraphType.java
@@ -3,5 +3,6 @@ package com.anwesome.app.soundvisualization.graphs;
 /**
  * Created by anweshmishra on 26/02/17.
  */
-public class VisualizingGraphType {
+public enum VisualizingGraphType {
+    BARGRAPH,CIRCLEGRAPH;
 }
