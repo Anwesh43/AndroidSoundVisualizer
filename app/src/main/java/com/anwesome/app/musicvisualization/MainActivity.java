a82646342b865e6b8c49d773884e2602bf66cae2
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 13:47:19 +0530

    Using circular graph

--- app/src/main/java/com/anwesome/app/musicvisualization/MainActivity.java
+++ app/src/main/java/com/anwesome/app/musicvisualization/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        player = new VisualizingPlayer(this,"surc.mp3", VisualizingGraphType.BARGRAPH);
+        player = new VisualizingPlayer(this,"surc.mp3", VisualizingGraphType.CIRCLEGRAPH);
         player.play();
     }
     public void onPause() {
