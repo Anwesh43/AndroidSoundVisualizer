2acaf5a7a959f1201f8cda22b4dd4df134438b86
Anwesh43 <anweshthecool0@gmail.com>
2017-02-28 13:35:29 +0530

    Adding dotlinegraph as a new value in enum VisualizingGraphType

--- soundvisualization/src/main/java/com/anwesome/app/soundvisualization/graphs/VisualizingGraphType.java
+++ soundvisualization/src/main/java/com/anwesome/app/soundvisualization/graphs/VisualizingGraphType.java
@@ -4,5 +4,5 @@ package com.anwesome.app.soundvisualization.graphs;
  * Created by anweshmishra on 26/02/17.
  */
 public enum VisualizingGraphType {
-    BARGRAPH,CIRCLEGRAPH;
+    BARGRAPH,CIRCLEGRAPH,DOTLINEGRAPH;
 }
